84d638d3d0bdafc34dd192de1485ba956ce89a2d
harpan <tgas.tik.harpan.9c@gmail.com>
2026-01-07 20:56:46 +0700

    Update users migration and add koperasi database structurev2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,16 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  public function run(): void
+{
+    // panggil seeder admin
+    $this->call(AdminSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    // optional: user dummy (boleh dihapus kalau tidak perlu)
+    User::factory()->create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+    ]);
+}
+
 }
